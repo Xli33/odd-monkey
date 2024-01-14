7f00ceecd91bfcafac40fdcd8eca85d22d27d3e9
Xli33 <lshin1232@gmail.com>
2024-01-14 23:11:37 +0800

    feat: 添加虎牙观看选项：自动最高画质，默认不启用

--- src/虎牙免登录观看.js
+++ src/虎牙免登录观看.js
@@ -2,12 +2,15 @@
 // @name        虎牙免登录观看
 // @description 虎牙未登录时不自动暂停，并隐藏进入页面后的登录框，解锁登录清晰度，若需要登录请勿使用！！
 // @author      (σ｀д′)σ
-// @version     1.1.1
+// @version     1.2.1
 // @namespace   https://greasyfork.org/zh-CN/scripts/477947
 // @license     GPL-3.0-or-later
 // @match       *://www.huya.com/*
 // @run-at      document-end
-// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_registerMenuCommand
+// @grant       GM_unregisterMenuCommand
 // @supportURL  https://greasyfork.org/zh-CN/scripts/477947
 // @homepageURL https://github.com/Xli33/odd-monkey
 // ==/UserScript==
@@ -17,6 +20,47 @@
 
   // 禁止虎牙篡改原生console
   // Object.freeze(console);
+
+  // 更改菜单选项
+  function changeMenuItem(id, onCaption, offCaption, onClick) {
+    const inplace = id === GM_registerMenuCommand(offCaption, null, { id });
+    if (inplace) {
+      // 插件如VM2.15.9起支持根据id注册选项
+      GM_registerMenuCommand(onCaption, onClick, { id });
+    } else {
+      // 不支持需先卸载原选项，再注册新选项
+      GM_unregisterMenuCommand(offCaption);
+      GM_registerMenuCommand(onCaption, onClick);
+    }
+  }
+  // 注册可变菜单项
+  function registerToggle(id, onCaption, offCaption, onClick) {
+    changeMenuItem(id, onCaption, offCaption, () => {
+      onClick();
+      registerToggle(id, offCaption, onCaption, onClick);
+    });
+  }
+
+  // 待注册
+  const toggles = [
+    {
+      id: 1,
+      title: "自动最高画质",
+      gmKey: "autoBestRES",
+      gmValue: GM_getValue("autoBestRES"),
+    },
+  ];
+  toggles.forEach((e) => {
+    registerToggle(
+      e.id,
+      (e.gmValue ? "✔️" : "✖️") + e.title,
+      (!e.gmValue ? "✔️" : "✖️") + e.title,
+      () => {
+        e.gmValue = !e.gmValue;
+        GM_setValue(e.gmKey, e.gmValue);
+      }
+    );
+  });
 
   window.addEventListener("load", () => {
     const getById = (id) => document.getElementById(id);
@@ -48,6 +92,8 @@
             .children(":has(.bitrate-right-btn.login-enjoy-btn)")
             .each((i, e) => {
               $(e).data("data").status = 0;
+              // 若启用了自动最高画质
+              i === 0 && toggles[0].gmValue && e.click();
             });
         };
         new MutationObserver(unlockRES).observe($vtList[0], {
